3fe2c1ff84cd6577d69c4f62ca4ecd364bd2ffec
MatchaLakers <a.zayn369@icloud.com>
2025-12-01 10:17:32 +1100

    Initial content

---
+++ loomi/loomi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  loomi
+//
+//  Created by Zayn on 1/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
