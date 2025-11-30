e452277a56d991776fdf16ae3bb6168d625393db
MatchaLakers <a.zayn369@icloud.com>
2025-12-01 10:21:13 +1100

    initial files

---
+++ loomi/loomi/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  loomi
+//
+//  Created by Zayn on 1/12/2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ loomi/loomi/ReviewView.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewView.swift
+//  loomi
+//
+//  Created by Zayn on 1/12/2025.
+//
+
+import SwiftUI
+
+struct ReviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReviewView()
+}
