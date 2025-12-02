82657eaaee4bba269d756381cc7d9935f379c738
MatchaLakers <a.zayn369@icloud.com>
2025-12-02 13:52:32 +1100

    working with json file on the stack

---
+++ loomi/loomi/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  loomi
+//
+//  Created by Zayn on 2/12/2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
