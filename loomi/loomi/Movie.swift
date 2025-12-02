93ea93f4c404013f8eee1b4cdab94b421c9eb8de
MatchaLakers <azifzaynsb@gmail.com>
2025-12-02 12:16:47 +1100

    Added the MovieView, JSON file and Movie.swift and MovieDataManager.swift to handle the data.

---
+++ loomi/loomi/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  loomi
+//
+//  Created by Janna Qian Zi Ng on 12/2/25.
+//
+
+import Foundation
---
+++ loomi/loomi/MovieDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  MovieDataManager.swift
+//  loomi
+//
+//  Created by Janna Qian Zi Ng on 12/2/25.
+//
+
+import Foundation
