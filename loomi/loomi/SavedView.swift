a3b72c4b3daf87c00d23df9dce5adf7beb1a085d
MatchaLakers <a.zayn369@icloud.com>
2025-12-02 12:32:34 +1100

    home page almost done

--- loomi/loomi/SavedView.swift
+++ loomi/loomi/SavedView.swift
@@ -1,18 +1,50 @@
 //
 //  SavedView.swift
-//  loomi
 //
-//  Created by Zayn on 1/12/2025.
+//
+//  Created by Andrew Wang on 2/12/2025.
 //
 
 import SwiftUI
 
 struct SavedView: View {
+    @State private var currentSearchText = ""
+    
+    private let posters = [
+        "jaws",
+        "godfather",
+        "batman",
+        "venture",
+        "avengers"
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            // TODO: Display the result in a list!
+            // Filter using `currentSearchText`!
+            // `LazyHGrid` `LazyVGrid`
+            ScrollView {
+                LazyVGrid(
+                    columns: [
+                        GridItem(.flexible(minimum: 100, maximum: 200)),
+                        GridItem(.flexible(minimum: 100, maximum: 200))
+                    ]
+                ) {
+                    ForEach(0..<21, id: \.self) { _ in
+                        // Image("")
+                        Rectangle()
+                            .fill(Color.blue)
+                            .aspectRatio(0.7, contentMode: .fit)
+                    }
+                }
+                .padding(8)
+            }
+            .navigationTitle("Saved Moies For You")
+        }
+        .searchable(text: $currentSearchText)
     }
 }
 
 #Preview {
-    SavedView()
+    RootTabView()
 }
